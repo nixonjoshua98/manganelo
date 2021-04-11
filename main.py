6ebb0502c982e6857e575cb2002dce06e13b4d15
joshuanixon <nixonjoshua98@gmail.com>
2021-04-11 10:40:08 +0100

    Version 1.6.1

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 
 if __name__ == "__main__":
-	from manganelo import SearchManga, MangaInfo, DownloadChapter
-
-	search = SearchManga("Attack on Titan", threaded=False)
-	results = search.results
-	best_result = results[0]
-	manga_info = MangaInfo(best_result.url, threaded=False)
-	manga_page = manga_info.results
-
-	file = f"./AoT {manga_page.chapters[0].title}.pdf"
-	dl = DownloadChapter(manga_page.chapters[0].url, file)
-	results = dl.results
-	if results.saved_ok:
-		print(results.path, results.percent_saved)
-
-	exit()
-
 	from manganelo import *
 
 	search = SearchManga("Raid", threaded=False)
@@ -29,6 +13,7 @@ if __name__ == "__main__":
 	manga_page = manga_info.results
 
 	for chapter in manga_page.chapters:
+		continue
 
 		file = f"./Raid {chapter.num}.pdf"
 
