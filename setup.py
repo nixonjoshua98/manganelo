292f8e4ccd7ddb8035087ebe604aef51bea3769e
joshuanixon <nixonjoshua98@gmail.com>
2021-05-29 11:56:47 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,36 @@ def read_file(file):
 setup(
 	name="manganelo",
 	packages=find_packages(),
-	version="dev",
+	version="1.18.3",
 	license="MIT",
 
-	description="Unofficial API for the Manganelo website.",
+	description="Unofficial API for the Manganelo/Manganato website.",
 	long_description=read_file("README.md"),
 	long_description_content_type="text/markdown",
 
 	author="Joshua Nixon",
+	author_email="joshuanixonofficial@gmail.com",
 
 	url="https://github.com/nixonjoshua98/manganelo",
+
+	download_url="https://github.com/nixonjoshua98/manganelo/archive/refs/tags/v1.18.3.tar.gz",
+
+	keywords=["manga", "manganelo", "scrapper", "web", "mangakakalot", "thread", "comic", "manhwa", "manganato"],
 
 	install_requires=[
 		"bs4",
 		"requests",
 		"reportlab",
 		"pillow"
+	],
+
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Build Tools",
 	],
 
 	python_requires='>=3.7'
